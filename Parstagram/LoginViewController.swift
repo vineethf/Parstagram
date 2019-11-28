40a0898bbc7a4e671ce231aafdf201dc71e4fccd
Vineeth Foregard <47223223+vineethf@users.noreply.github.com>
2019-11-28 02:05:30 -0500

    Add files via upload

---
+++ Parstagram/LoginViewController.swift
@@ -0,0 +1,51 @@
+//
+//  LoginViewController.swift
+//  Parstagram
+//
+//  Created by Vineeth on 11/15/19.
+//  Copyright © 2019 Vineeth. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var usernameInput: UITextField!
+    @IBOutlet weak var passwordInput: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func dismissKeyboard(_ sender: Any) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func signinBtn(_ sender: Any) {
+        PFUser.logInWithUsername(inBackground: usernameInput.text!, password: passwordInput.text!){
+            (user, error) in
+            if user != nil {
+                self.performSegue(withIdentifier: "loginSeg", sender: nil)
+                self.usernameInput.text = nil
+                self.passwordInput.text = nil
+            }else {
+                print("\(error)")
+            }
+            
+        }
+        
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Parstagram/SignUpPageViewController.swift
@@ -0,0 +1,59 @@
+//
+//  SignUpPageViewController.swift
+//  Parstagram
+//
+//  Created by Vineeth on 11/15/19.
+//  Copyright © 2019 Vineeth. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class SignUpPageViewController: UIViewController {
+
+    @IBOutlet weak var usernameInput: UITextField!
+    @IBOutlet weak var passwordInput: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func cancelBtn(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func dismissKeyboard(_ sender: Any) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func signupCompleteBtn(_ sender: Any) {
+        var user = PFUser()
+        user.username = usernameInput.text
+        user.password = passwordInput.text
+        
+        user.signUpInBackground { (sucess, error) in
+            if sucess   {
+                print("successfully signed up!")
+                self.usernameInput.text = nil
+                self.passwordInput.text = nil
+            }else{
+//                print("failed to sign up")
+                print(error)
+            }
+        }
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
